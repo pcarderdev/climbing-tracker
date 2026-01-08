95127c75329fdd12b957c9613a37b020c950e066
pat <31299967+pcarderdev@users.noreply.github.com>
2026-01-08 16:55:34 -0500

    added firebase config and session management

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAE4Iwuhu7UvldmAuiluLWYLI8r6U04B8I',
+  authDomain: 'climbing-tracker-d6c2a.firebaseapp.com',
+  projectId: 'climbing-tracker-d6c2a',
+  storageBucket: 'climbing-tracker-d6c2a.firebasestorage.app',
+  messagingSenderId: '287353383248',
+  appId: '1:287353383248:web:a9c5c9ab98e8569f706455',
+};
